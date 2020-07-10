59dfab92184cb7fc806ecc37a81ec4abf63fbb60
TrekFuel <ridge96@yandex.ru>
2020-07-10 16:56:36 +0300

    feat(project): add auth page with form (+validation)
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material-module';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store';
-import { LoginComponent } from './login/login.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material-module';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './store';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -20,9 +19,11 @@ import { LoginComponent } from './login/login.component';
     MaterialModule,
     StoreModule.forRoot(reducers, {
       metaReducers
-    })
+    }),
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  @ViewChild(FormGroupDirective, {static: true}) formGroupDirective:
+    FormGroupDirective;
+  hide = true;
 
-  constructor() { }
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this._initAuthForm();
+  }
+
+  private _initAuthForm() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50)
+      ]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  onLogin() {
+    console.log(this.email.value, this.password.value);
+    this.formGroupDirective.resetForm();
+  }
+
+  onHideShowClick() {
+    this.hide = !this.hide;
+    return false;
   }
 
 }
