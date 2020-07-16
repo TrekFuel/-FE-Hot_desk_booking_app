0d78fda734a3b6ed7e59917328e468619d546ea8
SergeySalimov <salimov_s@tut.by>
2020-07-16 11:18:39 +0300

    feat(project): added in shared app-clearable-input

---
+++ src/app/shared/clearable-input/clearable-input.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-clearable-input',
+  template: `
+    <mat-form-field appearance="outline">
+      <mat-label>{{label}}</mat-label>
+      <input matInput type="text" [(ngModel)]="value" [disabled]="disableStatus" (change)="onChange(value)">
+      <button mat-button *ngIf="value" matSuffix mat-icon-button aria-label="Clear" (click)="value=''">
+        <mat-icon>close</mat-icon>
+      </button>
+    </mat-form-field>
+  `,
+  styles: ['mat-form-field { width: 100% }'],
+})
+export class ClearableInputComponent {
+  @Input() label: string = 'Label';
+  @Input() disableStatus: boolean = false;
+  @Output() inputMessage = new EventEmitter<string>();
+  value: string = 'Clear me';
+  onChange(value: string) {
+    this.inputMessage.emit(value);
+  }
+}
