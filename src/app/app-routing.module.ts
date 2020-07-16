d71b9c45ee18f8ebd51a5b2b315bc4c3dc49e2aa
SergeySalimov <salimov_s@tut.by>
2020-07-16 10:12:39 +0300

    feat(project): did all preparations for creation rooms-management-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { UsersComponent } from "./layout/users/users.component";
 import { RoomsManagementEntryComponent } from './rooms-management/rooms-management-entry/rooms-management-entry.component';
+import { RoomsManagementEditComponent } from './rooms-management/rooms-management-edit/rooms-management-edit.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'users', component: UsersComponent },
   { path: 'rooms-management', component: RoomsManagementEntryComponent },
+  { path: 'rooms-management/edit', component: RoomsManagementEditComponent },
 ];
 
 @NgModule({
---
+++ src/app/rooms-management/rooms-management-edit/rooms-management-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-management-edit',
+  templateUrl: './rooms-management-edit.component.html',
+  styleUrls: ['./rooms-management-edit.component.scss']
+})
+export class RoomsManagementEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
