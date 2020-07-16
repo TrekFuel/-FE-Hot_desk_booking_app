f29ee1ac165470d46e4a2bac9098e821bd7fda6a
TrekFuel <ridge96@yandex.ru>
2020-07-16 16:29:13 +0300

    feat(project): add booking-page
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BookingRoutingModule} from './booking-routing.module';
 import {BookingPageComponent} from './booking-page/booking-page.component';
+import {SharedModule} from '../shared/shared.module';
+import {MaterialModule} from '../material/material-module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -9,6 +12,9 @@ import {BookingPageComponent} from './booking-page/booking-page.component';
   imports: [
     CommonModule,
     BookingRoutingModule,
+    SharedModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class BookingModule {
