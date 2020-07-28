56e416289455e1636b421a50020ade016fa17762
SergeySalimov <salimov_s@tut.by>
2020-07-28 13:30:08 +0300

    fix(hasFocus.direc):solved "Expression has changed after it was checked"

--- src/app/shared/directives/has-focus.directive.ts
+++ src/app/shared/directives/has-focus.directive.ts
@@ -3,19 +3,23 @@ import { AfterViewInit, Directive, ElementRef, Input, OnChanges, SimpleChanges }
 @Directive({
   selector: '[hasFocus]'
 })
-export class HasFocusDirective implements AfterViewInit, OnChanges{
+export class HasFocusDirective implements AfterViewInit, OnChanges {
   @Input("hasFocus") hasFocus: boolean;
   constructor(private elementRef: ElementRef) {}
 
   ngAfterViewInit() {
-    if (this.hasFocus) {
-      this.elementRef.nativeElement.focus();
-    }
+    setTimeout(() => {
+      if (this.hasFocus) {
+        this.elementRef.nativeElement.focus();
+      }
+    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes["hasFocus"] && changes["hasFocus"].currentValue === true) {
-      this.elementRef.nativeElement.focus();
-    }
+    setTimeout(() => {
+      if (changes['hasFocus']?.currentValue === true) {
+        this.elementRef.nativeElement.focus();
+      }
+    });
   }
 }
