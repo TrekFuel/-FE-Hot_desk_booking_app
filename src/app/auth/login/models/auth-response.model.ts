65d9d2e6031a94dce2fb7ccb33eb10f9cf45a06d
TrekFuel <ridge96@yandex.ru>
2020-07-28 14:45:26 +0300

    feat(project): integrate auth 2.0.0
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- src/app/auth/login/models/auth-response.model.ts
+++ src/app/auth/login/models/auth-response.model.ts
@@ -1,4 +1,12 @@
 export interface AuthResponse {
-  username: string;
+  userInfo: {
+    id: string,
+    email: string,
+    username: string,
+    firstName: string,
+    lastName: string,
+    isActive: boolean,
+    roleNames: string[]
+  };
   token: string;
 }
--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -1,16 +1,16 @@
-import { Effect, ofType } from '@ngrx/effects';
-import { AuthService } from '../../auth/login/services/auth.service';
-import { Injectable } from '@angular/core';
+import {Effect, ofType} from '@ngrx/effects';
+import {AuthService} from '../../auth/login/services/auth.service';
+import {Injectable} from '@angular/core';
 import {
   authActionType, LoginFailureAction,
   LoginStartAction, LoginSuccessAction, LogoutEndAction,
 } from '../actions/auth.actions';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { AuthResponse } from '../../auth/login/models/auth-response.model';
-import { environment } from '../../../environments/environment';
-import { HttpErrorResponse } from '@angular/common/http';
-import { of } from 'rxjs';
-import { Actions } from '@ngrx/effects';
+import {catchError, map, switchMap} from 'rxjs/operators';
+import {AuthResponse} from '../../auth/login/models/auth-response.model';
+import {environment} from '../../../environments/environment';
+import {HttpErrorResponse} from '@angular/common/http';
+import {of} from 'rxjs';
+import {Actions} from '@ngrx/effects';
 
 @Injectable()
 export class AuthEffects {
@@ -24,7 +24,15 @@ export class AuthEffects {
             map((response: AuthResponse) => {
 // user creation across all application
               const user: AuthResponse = {
-                username: response.username,
+                userInfo: {
+                  id: response.userInfo.id,
+                  email: response.userInfo.email,
+                  username: response.userInfo.username,
+                  firstName: response.userInfo.firstName,
+                  lastName: response.userInfo.lastName,
+                  isActive: response.userInfo.isActive,
+                  roleNames: [...response.userInfo.roleNames],
+                },
                 token: response.token,
               };
               localStorage.setItem(environment.localStorageUser, JSON.stringify(user));
