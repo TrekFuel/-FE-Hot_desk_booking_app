7670b576690c24be4b59037c82c766f9640e826c
TrekFuel <ridge96@yandex.ru>
2020-07-29 11:52:35 +0300

    fix(project): add AuthEffects import
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- src/app/store/effects/index.effects.ts
+++ src/app/store/effects/index.effects.ts
@@ -1,9 +1,11 @@
 import { UsersListEffects } from './usersList.effects';
+import { AuthEffects } from './auth.effects';
 import { LoaderEffects } from './loader.effects';
 import { MessageStateEffects } from './messageState.effects';
 
 export const indexEffects = [
   UsersListEffects,
+  AuthEffects,
   LoaderEffects,
   MessageStateEffects,
 ];
