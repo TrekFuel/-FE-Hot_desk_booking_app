a85cd97be78e01da08530c30b1c954a423e5d688
SergeySalimov <salimov_s@tut.by>
2020-08-01 14:23:33 +0300

    feat(office-choosing): step 1. Dynamic selects whithout input

--- src/app/shared/office-choosing/office-choosing.component.ts
+++ src/app/shared/office-choosing/office-choosing.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
-import { MatSelectChange } from '@angular/material/select';
-import { MyErrorStateMatcher, ValidateSameName } from '../validators/same-name.validator';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../validators/same-name.validator';
 import { SelectorsName } from './selectors-name';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SelectorsModel } from '../models/selectors.model';
+import { SelectorsAddress, SelectorsCity, SelectorsModel } from '../models/selectors.model';
+import { MatSelectChange } from '@angular/material/select';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-office-choosing',
@@ -74,41 +75,45 @@ export class OfficeChoosingComponent implements OnInit {
   floorArr: string[] = ['1', '2'];
   // -------------------
 
+  // new Life
+  currentChoice: { country: string | null, city: string | null, address: string | null, addressId: string } = {
+    country: null, city: null, address: null, addressId: ''
+  };
+
   canEditMode = true;
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   public get countryOptions(): string[] {
-    const country = [...this.countryArr];
-    if (this.canEditMode) {
-      this.addNewToSelect(country);
-    }
+    const country = [...this.selectorsModel.country];
     return country;
   }
 
   public get cityOptions(): string[] {
-    const city = [...this.cityArr];
-    if (this.canEditMode) {
-      this.addNewToSelect(city);
+    const city = [];
+    if (!!this.currentChoice.country) {
+      let country = this.currentChoice.country.toLowerCase();
+      this.selectorsModel.city.forEach((item: SelectorsCity) => {
+        if (item.country.toLowerCase() === country) {
+          city.push(item.city);
+        }
+      });
     }
-    return city;
+    return Array.from(new Set(city));
   }
 
   public get addressOptions(): string[] {
-    const address = [...this.addressArr];
-    if (this.canEditMode) {
-      this.addNewToSelect(address);
+    const address = [];
+    if (!!this.currentChoice.city) {
+      let city = this.currentChoice.city.toLowerCase();
+      this.selectorsModel.address.forEach((item: SelectorsAddress) => {
+        if (item.city.toLowerCase() === city) {
+          address.push(item.address);
+        }
+      });
     }
-    return address;
-  }
-
-  public get floorOptions(): string[] {
-    const floor = [...this.floorArr];
-    if (this.canEditMode) {
-      this.addNewToSelect(floor);
-    }
-    return floor;
+    return Array.from(new Set(address));
   }
 
   public get country(): AbstractControl {
@@ -123,166 +128,177 @@ export class OfficeChoosingComponent implements OnInit {
     return this.selectOfficeForm?.get('address');
   }
 
-  public get floor(): AbstractControl {
-    return this.selectOfficeForm?.get('floor');
-  }
-
-  public get inputNew(): AbstractControl {
-    return this.selectOfficeForm?.get('inputNew');
-  }
+  // public get floor(): AbstractControl {
+  //   return this.selectOfficeForm?.get('floor');
+  // }
+  //
+  // public get inputNew(): AbstractControl {
+  //   return this.selectOfficeForm?.get('inputNew');
+  // }
 
   ngOnInit() {
     this._initChoosingForm();
+    this.country.valueChanges.pipe(distinctUntilChanged())
+      .subscribe((country: string) => this.currentChoice = { country, city: null, address: null, addressId: '' });
+    this.city.valueChanges.pipe(distinctUntilChanged())
+      .subscribe((city: string) => this.currentChoice = { ...this.currentChoice, city, address: null, addressId: '' });
+    this.address.valueChanges.pipe(distinctUntilChanged())
+      .subscribe((city: string) => this.currentChoice = { ...this.currentChoice, city, address: null, addressId: '' });
+
   }
 
-  // ToDo in some case when you back your choice on selects, validators don`t turn off
-  toggleInputValidators(enable: boolean): void {
-    const inputValidators: ValidatorFn[] = [
-      Validators.required,
-      ValidateSameName(this.checkingInputNames)
-    ];
-    enable ? this.inputNew.setValidators(inputValidators) : this.inputNew.clearValidators();
-    this.inputNew.updateValueAndValidity();
-  }
-
+  // toggleInputValidators(enable: boolean): void {
+  //   const inputValidators: ValidatorFn[] = [
+  //     Validators.required,
+  //     ValidateSameName(this.checkingInputNames)
+  //   ];
+  //   enable ? this.inputNew.setValidators(inputValidators) : this.inputNew.clearValidators();
+  //   this.inputNew.updateValueAndValidity();
+  // }
+  //
   onOpenSelect(source: string): void {
+
     // for correct work of double click on same select to (selectionChange) work
-    switch (source) {
-      case this.SelectorsName.country:
-        this.selectOfficeForm.patchValue({ country: null });
-        break;
-      case this.SelectorsName.city:
-        this.selectOfficeForm.patchValue({ city: null });
-        break;
-      case this.SelectorsName.address:
-        this.selectOfficeForm.patchValue({ address: null });
-        break;
-      case this.SelectorsName.floor:
-        this.selectOfficeForm.patchValue({ floor: null });
-        break;
-      default:
-        break;
-    }
-
-    this.currentFocus = null;
-    if (this.canEditMode) {
-      this.resetInput();
-    }
-
-    if (source !== SelectorsName.floor) {
-      this.selectOfficeForm.patchValue({ floor: null });
-      this.floor.disable();
-      if (source !== SelectorsName.address) {
-        this.selectOfficeForm.patchValue({ address: null });
-        this.address.disable();
-        if (source !== SelectorsName.city) {
-          this.selectOfficeForm.patchValue({ city: null });
-          this.city.disable();
-        }
-      }
-    }
+    // switch (source) {
+    //   case this.SelectorsName.country:
+    //     this.selectOfficeForm.patchValue({ country: null });
+    //     break;
+    //   case this.SelectorsName.city:
+    //     this.selectOfficeForm.patchValue({ city: null });
+    //     break;
+    //   case this.SelectorsName.address:
+    //     this.selectOfficeForm.patchValue({ address: null });
+    //     break;
+    //   case this.SelectorsName.floor:
+    //     this.selectOfficeForm.patchValue({ floor: null });
+    //     break;
+    //   default:
+    //     break;
+    // }
+    //
+    // this.currentFocus = null;
+    // if (this.canEditMode) {
+    //   this.resetInput();
+    // }
+    //
+    // if (source !== SelectorsName.floor) {
+    //   this.selectOfficeForm.patchValue({ floor: null });
+    //   this.floor.disable();
+    //   if (source !== SelectorsName.address) {
+    //     this.selectOfficeForm.patchValue({ address: null });
+    //     this.address.disable();
+    //     if (source !== SelectorsName.city) {
+    //       this.selectOfficeForm.patchValue({ city: null });
+    //       this.city.disable();
+    //     }
+    //   }
+    // }
   }
 
+  //
   onSelected(selection: MatSelectChange): void {
-    let value: string = selection.value.toString().toLowerCase() || '';
-    let source: string = selection.source.ngControl.name.toString().toLowerCase() || '';
-
-
-    if (value === SelectorsName.new && this.canEditMode) {
-      this.newSelected = source;
-      this.inputNew.enable();
-      this.currentFocus = SelectorsName.new;
-      this.toggleInputValidators(true);
-    } else {
-      this.enableNextSelection();
-    }
+    //   let value: string = selection.value.toString().toLowerCase() || '';
+    //   let source: string = selection.source.ngControl.name.toString().toLowerCase() || '';
+    //
+    //
+    //   if (value === SelectorsName.new && this.canEditMode) {
+    //     this.newSelected = source;
+    //     this.inputNew.enable();
+    //     this.currentFocus = SelectorsName.new;
+    //     this.toggleInputValidators(true);
+    //   } else {
+    //     this.enableNextSelection();
+    //   }
+    // }
+    //
+    // enableNextSelection(): void {
+    //   if (this.country?.value && this.country.value.toLowerCase() !== SelectorsName.new) {
+    //     this.city.enable();
+    //     this.currentFocus = SelectorsName.city;
+    //     if (this.city?.value && this.city.value.toLowerCase() !== SelectorsName.new) {
+    //       this.address.enable();
+    //       this.currentFocus = SelectorsName.address;
+    //       if (this.address?.value && this.address.value.toLowerCase() !== SelectorsName.new) {
+    //         this.floor.enable();
+    //         this.currentFocus = SelectorsName.floor;
+    //       }
+    //     }
+    //   }
   }
 
-  enableNextSelection(): void {
-    if (this.country?.value && this.country.value.toLowerCase() !== SelectorsName.new) {
-      this.city.enable();
-      this.currentFocus = SelectorsName.city;
-      if (this.city?.value && this.city.value.toLowerCase() !== SelectorsName.new) {
-        this.address.enable();
-        this.currentFocus = SelectorsName.address;
-        if (this.address?.value && this.address.value.toLowerCase() !== SelectorsName.new) {
-          this.floor.enable();
-          this.currentFocus = SelectorsName.floor;
-        }
-      }
-    }
-  }
-
-  onInputMessage(source: string): void {
-    const value = this.inputNew?.value;
-    if (this.selectOfficeForm.valid) {
-      switch (source) {
-        case this.SelectorsName.country:
-          this.countryArr.push(value);
-          this.selectOfficeForm.patchValue({ country: null });
-          break;
-        case this.SelectorsName.city:
-          this.cityArr.push(value);
-          this.selectOfficeForm.patchValue({ city: null });
-          break;
-        case this.SelectorsName.address:
-          this.addressArr.push(value);
-          this.selectOfficeForm.patchValue({ address: null });
-          break;
-        case this.SelectorsName.floor:
-          this.floorArr.push(value);
-          this.selectOfficeForm.patchValue({ floor: null });
-          break;
-        default:
-          break;
-      }
-      this.enableNextSelection();
-      this.resetInput();
-    }
-  }
-
-  resetInput(): void {
-    this.inputNew.disable();
-    this.inputNew.reset(null);
-    this.toggleInputValidators(false);
-    this.newSelected = null;
-  }
-
-  addNewToSelect(arr: string[]): void {
-    arr.unshift(SelectorsName.new[0].toUpperCase() + SelectorsName.new.substring(1));
-  }
-
-  onClickShowHide() {
-    this.isShowMap = !this.isShowMap;
-    this.showMap.emit(this.isShowMap);
-  }
-
+  //
+  // onInputMessage(source: string): void {
+  //   const value = this.inputNew?.value;
+  //   if (this.selectOfficeForm.valid) {
+  //     switch (source) {
+  //       case this.SelectorsName.country:
+  //         this.countryArr.push(value);
+  //         this.selectOfficeForm.patchValue({ country: null });
+  //         break;
+  //       case this.SelectorsName.city:
+  //         this.cityArr.push(value);
+  //         this.selectOfficeForm.patchValue({ city: null });
+  //         break;
+  //       case this.SelectorsName.address:
+  //         this.addressArr.push(value);
+  //         this.selectOfficeForm.patchValue({ address: null });
+  //         break;
+  //       case this.SelectorsName.floor:
+  //         this.floorArr.push(value);
+  //         this.selectOfficeForm.patchValue({ floor: null });
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //     this.enableNextSelection();
+  //     this.resetInput();
+  //   }
+  // }
+  //
+  // resetInput(): void {
+  //   this.inputNew.disable();
+  //   this.inputNew.reset(null);
+  //   this.toggleInputValidators(false);
+  //   this.newSelected = null;
+  // }
+  //
+  // addNewToSelect(arr: string[]): void {
+  //   arr.unshift(SelectorsName.new[0].toUpperCase() + SelectorsName.new.substring(1));
+  // }
+  //
+  // onClickShowHide() {
+  //   this.isShowMap = !this.isShowMap;
+  //   this.showMap.emit(this.isShowMap);
+  // }
+  //
   onSubmit() {
-    if (this.selectOfficeForm.valid && !this.newSelected) {
-      const queryParams = { ...this.selectOfficeForm.value };
-      if (this.canEditMode) {
-        delete queryParams.inputNew;
-      }
-      this.router.navigate(['.'], {
-        relativeTo: this.route,
-        queryParams,
-        queryParamsHandling: 'merge' // remove to replace all query params by provided
-        // replaceUrl: true // If we want to replace it in the history instead of adding new value there
-      });
-      console.log(queryParams);
-    }
+    console.log(this.selectOfficeForm.value);
+
+    // if (this.selectOfficeForm.valid && !this.newSelected) {
+    //   const queryParams = { ...this.selectOfficeForm.value };
+    //   if (this.canEditMode) {
+    //     delete queryParams.inputNew;
+    //   }
+    //   this.router.navigate(['.'], {
+    //     relativeTo: this.route,
+    //     queryParams,
+    //     queryParamsHandling: 'merge' // remove to replace all query params by provided
+    //     // replaceUrl: true // If we want to replace it in the history instead of adding new value there
+    //   });
+    //   console.log(queryParams);
+    // }
   }
 
   private _initChoosingForm(): void {
     this.selectOfficeForm = new FormGroup({
-      country: new FormControl({ value: null, disabled: false }, Validators.required),
-      city: new FormControl({ value: null, disabled: true }, Validators.required),
-      address: new FormControl({ value: null, disabled: true }, Validators.required),
-      floor: new FormControl({ value: null, disabled: true }, Validators.required),
-      inputNew: new FormControl({ value: null, disabled: true })
+      country: new FormControl(null, Validators.required),
+      city: new FormControl(null, Validators.required),
+      address: new FormControl(null, Validators.required)
+      // floor: new FormControl({ value: null, disabled: true }, Validators.required),
+      // inputNew: new FormControl({ value: null, disabled: true })
     });
-    if (!this.canEditMode) {
-      this.selectOfficeForm.removeControl('inputNew');
-    }
+    // if (!this.canEditMode) {
+    //   this.selectOfficeForm.removeControl('inputNew');
+    // }
   }
 }
