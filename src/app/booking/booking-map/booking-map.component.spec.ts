fd4ab12a1883f3b99cd9154f4e9363185a2d1086
SergeySalimov <salimov_s@tut.by>
2020-08-05 15:34:56 +0300

    feat(booking-page): added booking-map component + container

---
+++ src/app/booking/booking-map/booking-map.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookingMapComponent } from './booking-map.component';
+
+describe('BookingMapComponent', () => {
+  let component: BookingMapComponent;
+  let fixture: ComponentFixture<BookingMapComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookingMapComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookingMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/booking/booking-map/booking-map.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-map',
+  templateUrl: './booking-map.component.html',
+  styleUrls: ['./booking-map.component.scss']
+})
+export class BookingMapComponent {
+
+  constructor() {
+  }
+
+}
