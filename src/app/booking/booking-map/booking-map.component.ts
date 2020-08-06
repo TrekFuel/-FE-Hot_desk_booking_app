b485a0d5001542d2654ff96480b046720208c564
SergeySalimov <salimov_s@tut.by>
2020-08-06 23:17:44 +0300

    feat(booking-map): added eventListeners on places

--- src/app/booking/booking-map/booking-map.component.ts
+++ src/app/booking/booking-map/booking-map.component.ts
@@ -1,16 +1,17 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CanvasSize } from '../../rooms-management/rooms-management-edit/models/editor-blocks.models';
 import { Canvas } from 'fabric/fabric-impl';
 import { fabric } from 'fabric';
 import { CANVAS_DEFAULT, CANVAS_OPTION } from '../../rooms-management/rooms-management-edit/canvas-option';
 import { MOCK_OFFICE } from '../../shared/mock-office';
+import { EDITOR_NAMES } from '../../rooms-management/rooms-management-edit/editor-blocks-info';
 
 @Component({
   selector: 'app-booking-map',
   templateUrl: './booking-map.component.html',
   styleUrls: ['./booking-map.component.scss']
 })
-export class BookingMapComponent implements OnInit {
+export class BookingMapComponent implements OnInit, OnDestroy {
 
   @ViewChild('htmlCanvasBooking', { static: true }) htmlCanvas: ElementRef;
   public canvasSize: CanvasSize = CANVAS_DEFAULT;
@@ -26,6 +27,36 @@ export class BookingMapComponent implements OnInit {
   ngOnInit(): void {
     this._initCanvas();
     this.loadMap();
+
+    this.canvas.on({
+      'mouse:over': (e) => {
+        const actObj: fabric.Object = e.target;
+        if (actObj?.name === EDITOR_NAMES.place && this.canvas.getActiveObjects().length <= 1) {
+          this.canvas.hoverCursor = 'pointer';
+          console.log('place under mouse');
+          //   this.currentPlace.isPlaceHovered = true;
+          //   this.currentPlace.placeData = actObj.data;
+        }
+      },
+      'mouse:out': (e) => {
+        this.canvas.hoverCursor = 'default';
+        // const actObj: fabric.Object = e.target;
+      },
+      'mouse:down': (e) => {
+        const actObj: fabric.Object = e.target;
+        if (actObj?.name === EDITOR_NAMES.place) {
+          console.log('click on place');
+          //   this.currentPlace.isPlaceClickedToClose = false;
+          //   this.currentPlace.placeData = actObj.data;
+          // } else {
+          //   this.hidePlaceData();
+        }
+      },
+      'mouse:down:before': (e) => {
+        // this.blockedElements.includes(e.target?.name) ? this.discardActObj()
+        // : this.positioningCloneAndClose(e.target)
+      }
+    });
   }
 
   doCanvasZoom(zoom: number = this.canvasSize.zoom) {
@@ -51,6 +82,10 @@ export class BookingMapComponent implements OnInit {
       obj.hasBorders = false;
       obj.selectable = false;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.canvas.off();
   }
 
   private _initCanvas(): void {
